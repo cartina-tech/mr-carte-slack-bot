988788cc623375fdefabe009017e9336e56992ee
Alexander Forrest <alex@alexdev.se>
2023-04-18 11:45:57 +0200

    gitignore updated

---
+++ data/store.ts
@@ -0,0 +1,119 @@
+export const tacoGifs = [
+  "https://giphy.com/gifs/taco-tacos-party-3o7WTIGSUtYwGC1YNq",
+  "https://giphy.com/gifs/DefyTVNetwork-duck-dynasty-defytv-o58sdCOY7HPPZEBofg",
+  "https://giphy.com/gifs/cityoflasvegas-tacos-taco-tacotuesday-RLzQEesktlOd260mqU",
+  "https://giphy.com/gifs/VdUMZWjOYOsRa",
+  "https://giphy.com/gifs/taco-tacos-tuesday-26uf4WftccToOFm8w",
+  "https://giphy.com/gifs/tacobell-taco-tacos-bell-3o7ZezGPktFNZj93os",
+  "https://giphy.com/gifs/dancing-food-taco-3o6ZtkmiFtpBvii6uQ",
+  "https://giphy.com/gifs/tacobell-taco-tacos-bell-l0NwCudcnztLselXO",
+  "https://giphy.com/gifs/times-bell-orientation-7if9hGmIHjrTG",
+];
+
+export const wiseText = [
+  "Den som gräver en grop åt andra faller ofta själv däri. :face_with_hand_over_mouth:",
+  "Den som söker hen finner. :mag:",
+  "Galna katter får rivet skinn. :pouting_cat:",
+  "Bättre lyss till den sträng som brast än aldrig spänna en båge. :bow_and_arrow:",
+  "Det är ingen ko på isen (så länge rumpan är i land).",
+  'Det är mänskligt att fela. "Errare humanum est" :man-bowing:',
+  "Det är saligare att giva än att taga. :guru:",
+  "De synder man mest ångrar på gamla dar är de man inte begått. :bulb:",
+  "Alla är vi barn i början. :woman_feeding_baby:",
+  "Ingenting är nytt under solen. :sunny:",
+  "Lärdom är mer värt än guld. :first_place_medal:",
+  "Nya kvastar sopar bäst. :broom:",
+  "Rom byggdes inte på en dag. :it:",
+  "Synden straffar sig själv. :no_good:",
+  "Tron kan försätta berg. :thinking_face:",
+  "Var dag har nog av sin egen plåga. :thinking_face:",
+  "Var sak har sin tid. :clock11:",
+  "Även dåren anses vis om han tiger. :tiger:",
+  "Borta bra men hemma bäst. :house_with_garden:",
+  "Bättre en fågel i handen än tio i skogen. :hatching_chick:",
+  "Bättre stämma i bäcken än i ån. :ocean:",
+  "Det dunkelt sagda är det dunkelt tänkta. :thinking_face:",
+  "Först till kvarn får först mala. :thinking_face:",
+  "Slösa i guldmynt, hushålla i strösand. :money_bag:",
+  "Små brunnar släcka ock törsten. :potable_water:",
+  "Små hundar göra ofta största skadan. :dog2:",
+  "Bättre förekomma, än förekommas. :thinking_face:",
+  "Bättre lida orätt, än göra orätt. :thinking_face:",
+  "Bättre mager soppa, än intet doppa. :bowl_with_spoon:",
+  "Bättre sent än aldrig. :thinking_face:",
+  "Bättre små fiskar, än tomma diskar. :fish:",
+  "Bättre något släppa, än allt mista. :thinking_face:",
+];
+
+export const salutations = [
+  "Hej",
+  "Tjenixen",
+  "Hallå",
+  "Hej svejs",
+  "God dagens",
+  "Tjena",
+  "Tjabba",
+  "Hejsan",
+  "Tjenare",
+  "Tjoflöjt",
+  "Hallå där",
+];
+
+export const congrats = [
+  "Snurrande mustascher!",
+  "Milda matilda",
+  "Som ur en hipsters liv",
+  "Dra mig baklänges..",
+  "Underbart",
+  "Schnyggt",
+  "Nåja",
+  "Hey ho, let's go",
+  "It's hammer-time",
+  "Nu kliade det i mustaschen",
+  "Aha!",
+];
+
+export const jokes = [
+  "Två rika män stod och skröt om vem som var rikast.\n\n- Jag är så rik att jag kan köpa hela jordklotet!\n\n- Nej det kan du inte, för jag säljer det inte.",
+  "- Min katt spelar schack.\n\n- Det måste vara en intelligent katt.\n\n- Nej, inte särskilt. Jag vinner alltid.",
+  "Putte visar sitt nya hem för några av sina vänner.\n\n- Jag har inrett det helt efter eget huvud.\n\n- Ja, det verkar lite tomt.",
+  "- Jag låter alltid några lampor vara tända under natten för tjuvarna.\n\n- Det är ju slöseri med elektriciteten. De brukar väl alltid ha med sig ficklampor?",
+  "Pappan till sin son:\n\n- Men Niklas, varför bet du magistern i benet?\n\n- Han behandlade mig som en hund.",
+  "Vad heter du lille vän?\n\n- Tom.\n\n- Är det efter pappa?\n\n- Nej, efter hans plånbok.",
+  "- Hovmästaren! ska vi sitta här tills vi svälter ihjäl??\n\n- Det går tyvärr inte. Vi stänger klockan 22.00",
+  "Lars Ström tog en tur med sin nya elbil. Plötsligt fick han sladd, tappade kontakten och slog en volt.",
+  "- Vad skulle du göra om du spelade piano som jag?\n\nTa kontakt med en pianolärare och ta lektioner.",
+  "- Vad gör du i skåpet Per?\n\n- Leker periskop.",
+  "- Det var en rolig liten båt!\n\n- Ja, jag tror den driver med oss!",
+  "Allan kunde inte hitta E6:an så han körde E3:an två gånger istället.",
+  "- Nu måste du gå till skolan Nisse!\n\n- Nej, jag vill inte! alla är så dumma mot mig där!\n\n- Gå nu! du är ju i alla fall rektor!",
+  "Pappa frågar sin dotter:\n\n- Ser du bra med dina nya glasögon?\n\n- Visst, mamma!",
+  "Pelle skryter för Per:\n\n- I vår trappuppgång bor det en pojke som går på lina.\n\n- Det var väl inget. I vår trappuppgång finns det fem tanter som springer på kafferep.",
+  "- Mamma, varför är det synd om myror?\n\n- Det är väl inte synd om myror?\n\n- Jo, fröken sade att vi skulle gå ut och titta på myrstackarna",
+  "- Vet du vilka jag såg idag?\n\n- Nej.\n\n- Alla jag tittade på.",
+  "- Vad jobbar du med?\n\n- Jag är gatuförsäljare.\n\n- Så intressant! Vad tar ni för en gata nu för tiden?",
+  "Frustration: När man har köpt en ny boomerang och märker att det inte går att kasta bort den gamla...",
+  "Låna alltid pengar från en pessimist. Han räknar inte med att få dom tillbaka.",
+  "Det står att när de skulle baka pannkakor i fängelset använde de en fånge som smet.",
+  "- Varför kom inte Tomten till er i år?\n\n- Han hade klappat ihop.",
+  "Pessimisten: Värre än så här kan det inte bli!\n\nOptimisten: Jodå! Vänta bara!",
+  "Nisse söker plats som försäljare i klockbutiken.\n\n- Har du någon vana?\n\n- Ja. Till mitt nuvarande jobb pendlar jag varje dag...",
+  '- Sedan lille Kurt lärde sig läsa är han sysselsatt i flera timmar.\n\n- Hur då?\n\n- Jag ger honom en lapp där det står "Titta på andra sidan!" på båda sidor.',
+  "- Pappa, mjölken går ut i morgon.\n\n- Då får vi väl låsa in den då.",
+  "- Håll ögonen öppna!\n\n- Varför det?\n\n- Annars ser du inget!",
+  "Vet du varför Björn Borg inte får vara i en bokhandel?\n\n- Han tar för många gem.",
+  "Det var två norrmän som var ute och fiskade. Den ena fick napp och drog upp den.\n\n- Nå.. hur ska vi döda den?\n\n- Jag vet, vi dränker den!!!!",
+  "Två myggor åker på en moped. Plötsligt stannar de. Den som sitter bakom frågar den som kör:\n\n- Varför stannar du?\n\n- Jag fick en fluga i ögat.",
+  " Varför ramlar de där ungarna hela tiden?\n\n- De är ju trillingar!",
+];
+
+// default greeting
+export const assistantGreeting =
+  "Jag heter *Mr. Carpe (Diem)*, Cartinas egna assistent. Se så, fråga mig vad du vill!";
+
+// scheduled messages
+/* time report */
+export const timeReportNotice =
+  `>Vill bara vänligen påminna att det är dags att tidsrapportera! Klicka in och fyll i tidrapport här <${
+    Deno.env.get("TICTAC_LOGIN_URL")
+  }|Tic Tac> :clock7: :memo:`;
